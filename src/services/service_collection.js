6d4084117bbbe300974d013e150edcaa93181170
harut <harutk7@gmail.com>
2024-02-15 15:35:57 +0400

    updates

--- src/services/service_collection.js
+++ src/services/service_collection.js
@@ -75,37 +75,11 @@ class ServiceCollection {
 
 const serviceCollection = new ServiceCollection();
 
-const AuthorizeService = require('./payment/merchant/authorize.service');
-const StripeService = require('./payment/merchant/stripe.service');
-const CloverService = require('./payment/merchant/clover.service');
-const PaymentObjectValidator = require('./payment/payment_object_validator.service');
-const EasyshipService = require('./shiping/merchant/easyship.service');
-const CheckeeperService = require('./payment/merchant/checkeeper.service');
-const TaxjarService = require('./payment/merchant/taxjar.service');
-const SocketService = require('./socket/ws.services');
-const ExcelService = require('./excel/excel.service');
-const AxiosService = require('./shared/axios.service');
 const TimezoneService = require('./shared/timezone.service');
-const AggregationService = require('./shared/aggregation.service');
-const EventBusService = require('./event_bus/eventbusService.service');
 const RedisCache = require('./cache/RedisCache');
 const TelegramBotService = require('./telegram/telegram_bot.service');
 
-serviceCollection.addTransient('authorizeService', AuthorizeService, {});
-serviceCollection.addTransient('stripeService', StripeService, {});
-serviceCollection.addTransient('cloverService', CloverService, {});
-serviceCollection.addTransient('paymentObjectValidator', PaymentObjectValidator, {});
-serviceCollection.addTransient('easyshipService', EasyshipService, {});
-serviceCollection.addTransient('checkeeperService', CheckeeperService, {});
-serviceCollection.addTransient('taxjarService', TaxjarService, {});
-serviceCollection.addTransient('excelService', ExcelService, {});
-serviceCollection.addTransient('axiosService', AxiosService, {});
 serviceCollection.addTransient('timezoneService', TimezoneService, {});
-serviceCollection.addTransient('aggregationService', AggregationService, {});
-
-serviceCollection.addSingleton('socketService', SocketService, {});
-serviceCollection.addSingleton('receiverEventBusService', EventBusService, {});
-serviceCollection.addSingleton('publisherEventBusService', EventBusService, { connect: true });
 serviceCollection.addSingleton('redisCacheStore', RedisCache, { connect: true });
 serviceCollection.addSingleton('telegramBotService', TelegramBotService, {});
 
