0afced08129cf9fd0f5cec4accd3ef1f6bc8ca03
Hakobyan21 <hakobyanmarat484@gmail.com>
2024-03-04 18:56:58 +0400

    querybuilder functional bug fix

--- src/repository/chat_members/index.js
+++ src/repository/chat_members/index.js
@@ -1,104 +1,175 @@
 const db = require("../../../db.pg");
 const dbConstants = require("../../constants/db.config");
 
-const table = db.table(dbConstants.tables.chatMembers);
+const table = db.table(dbConstants.tables.conversations);
 const result = (data, error) => ({ error, data });
-
+const queryBuilder = require('../../helpers/pg.query')
 const create = async (body) => {
-  const member = await db.table(dbConstants.tables.chatMembers).insert(body).returning("*");
-  return result(member[0]);
+  if (!body.name) return result(null, "missing name");
+  if (!body.type) return result(null, "missing type");
+  if (!body.provider) return result(null, "missing provider");
+  return await db
+    .table(dbConstants.tables.conversations)
+    .insert(body)
+    .returning("*");
+};
+const getConversation = async (member, target) => {
+  console.log(member,"adsfdf");
+  let conversation = await db
+    .table(dbConstants.tables.conversations)
+    .select()
+    .whereRaw(`members @> '${JSON.stringify([member.id, target.id])}'::jsonb`)
+    .where({ deleted: 0 })
+    .returning("id");
+
+
+  if (!conversation.length) {
+    conversation = await db
+      .table(dbConstants.tables.conversations)
+      .insert({
+        name: target.name,
+        type: "single",
+        provider: member.provider,
+        members: JSON.stringify([member.id, target.id]),
+      })
+      .returning("id");
+  }
+  const conversationId = conversation[0].id
+
+  conversation = await db.raw(queryBuilder.selectConversationsByMembersByIdQuery(conversationId)).then(res => res.rows[0])
+  console.log(conversation,7898456);
+  return result(conversation);
 };
 
-const findByUserId = async (id) => {
-  const member = await db.table(dbConstants.tables.chatMembers)
-    .where(function () {
-      this.where("client_id", id).orWhere("user_id", id);
-    })
-    .select("*");
-  return result(member[0]);
-};
-
-const getMemberByClientId = async (id) => {
-  const member = await db
-    .table(dbConstants.tables.chatMembers)
-    .where({ id })
-    .select('*')
-  return result(member[0]);
+const getByMongoProvider = async (mongoProvider) => {
+  console.log(mongoProvider,"mongoProvider");
+  let provider = await db
+    .table(dbConstants.tables.providers)
+    .where({ mongo_id: mongoProvider._id.toString() })
+  if (provider.length === 0) {
+    provider = await db
+      .table(dbConstants.tables.providers)
+      .insert({
+        mongo_id: mongoProvider._id.toString(),
+        name: mongoProvider.name[0].name
+      })
+      .returning('*')
+  }
+  return  result(provider[0])
 }
 
-const getMemberByIdOrClientId = async (id) => {
-  const member = await db.table(dbConstants.tables.chatMembers)
-    .where(function () {
-      this.where("id", id).orWhere("client_id", id);
-    })
+const registerClientConversation = async (id, body) => {
+  let conversation = await db
+    .table(dbConstants.tables.conversations)
+    .whereRaw(`members @> '${JSON.stringify([id])}'::jsonb`)
     .select("*");
-  return result(member[0]);
-};
 
-const find = async (search) => {
-  const members = await db.table(dbConstants.tables.chatMembers)
-  .where(function () {
-    this.whereRaw(`exists (select 1 from jsonb_array_elements(phones) as elem where elem->>'phoneNumber' like ?)`, [`%${search}%`]).orWhere(
-      "name",
-      "ILIKE",
-      `%${search}%`
-    );
-  })
-  .select("*");
-  return result(members);
-};
-
-const registerUserMember = async (user, provider) => {
-  let member = await db.table(dbConstants.tables.chatMembers).where({ user_id: user._id.toString() }).returning("*");
-  if (!member.length) {
-    member = await db.table(dbConstants.tables.chatMembers)
-      .insert({
-        user_id: user._id.toString(),
-        provider: provider.id,
-        avatar: user.avatar,
-        name: user.firstname + " " + user.lastname,
-        phones: JSON.stringify([user.phone]),
-      })
+  if (!conversation.length) {
+    conversation = await db
+      .table(dbConstants.tables.conversations)
+      .insert(body)
       .returning("*");
   }
-
-  return member[0];
+  return conversation[0];
 };
-
-const registerMember = async (chatMember) => {
-  let member = await db.table(dbConstants.tables.chatMembers)
-    .where(function () {
-      this.where("client_id", user._id).orWhere("user_id", user._id);
-    })
-    .select("id")[0];
-  if (!member) {
-    member = await db.table(dbConstants.tables.chatMembers).insert(chatMember).returning("id")[0];
-  }
-  return result(member.id);
-};
-
-const findById = async (id) => {
-  const member = await db.table(dbConstants.tables.chatMembers).where({ id }).select("*");
-  return result(member[0]);
-};
-
-const getList = async (filter, limit = 10, page = 1) => {
-  const list = await db.table(dbConstants.tables.chatMembers)
-    .where(filter)
-    .select("*")
+const getUsersList = async (ids, limit = 10, page = 1) => {
+    const conversationsIdList = await db
+    .table(dbConstants.tables.conversations)
+    .select('id')
+    .whereRaw(`members @> '${ids}'::jsonb`)
+    .where({ deleted: 0 })
     .limit(limit)
     .offset((page - 1) * limit);
+
+    const list = await db.raw(queryBuilder.selectConversationsByMembersByIdsQuery(conversationsIdList.map(item => item.id))).then(res => res.rows)
+    return result(list);
+};
+
+// const getUsersList = async (ids, limit = 10, page = 1) => {
+//   const list = await db
+//     .table(dbConstants.tables.conversations)
+//     .select()
+//     .whereRaw(`members @> '${ids}'::jsonb`)
+//     .where({ deleted: 0 })
+//     // .innerJoin(db.raw("jsonb_array_elements_text(conversations.members)::integer as member_id"), 'member_id', 'chat_members.id')
+//     .limit(limit)
+//     .offset((page - 1) * limit);
+//   return result(list);
+// };
+
+const getList = async (filter, limit = 10, page = 1) => {
+  let list = await db
+    .table(dbConstants.tables.conversations)
+    .select('id')
+    .where({ ...filter, deleted: 0 })
+    .limit(limit)
+    .offset((page - 1) * limit);
+
+  // Extract member IDs from the list
+  const memberIds = list.map(item => item.id) // Assuming members is an array of member IDs
+  console.log(memberIds,455623);
+  list = await db.raw(queryBuilder.selectConversationsByMembersByIdsQuery(memberIds)).then(res => res.rows)
+
   return result(list);
+};
+
+// const getList = async (filter, limit = 10, page = 1) => {
+//   const list = await db
+//     .table(dbConstants.tables.conversations)
+//     .select()
+//     .where({ ...filter, deleted: 0 })
+//     .limit(limit)
+//     .offset((page - 1) * limit);
+//   return result(list);
+// };
+
+const getByMongoId = async (mongo_id) => {
+  const provider = await db
+    .table(dbConstants.tables.providers)
+    .where({ mongo_id })
+    .select('*')
+  return result(provider[0])
+}
+const deleteConversation = async (id) => {
+  const deletedList = await db
+    .table(dbConstants.tables.conversations)
+    .where({ id })
+    .update({ deleted: 1, deleted_at: new Date() });
+  return result(deletedList);
+};
+
+const getById = async (id) => {
+  const conversation = await db
+    .table(dbConstants.tables.conversations)
+    .where({ id })
+    .select("*")
+    .leftJoin("chat_members", function () {
+      this.on(
+        "chat_members.id",
+        "in",
+        knex.raw("jsonb_array_elements_text(conversations.members)::integer")
+      );
+    });
+  return conversation[0];
+};
+
+const update = async (id, body) => {
+  const updatedList = await db
+    .table(dbConstants.tables.conversations)
+    .where({ id })
+    .update(body);
+  return result(updatedList);
 };
 
 module.exports = {
   create,
-  findByUserId,
-  getMemberByIdOrClientId,
-  find,
-  registerUserMember,
-  registerMember,
-  findById,
-  getMemberByClientId,
+  getConversation,
+  getUsersList,
   getList,
+  deleteConversation,
+  registerClientConversation,
+  getByMongoProvider,
+  update,
+  getById,
+  getByMongoId,
 };
